6791b6f78897c69b525ddc54974c39319d1646aa
Vadim Ogievetsky <vadimon@gmail.com>
2017-10-11 18:31:53 -0700

    larger timeout

--- test/druidRequesterLive.mocha.js
+++ test/druidRequesterLive.mocha.js
@@ -28,7 +28,7 @@ let druidRequester = druidRequesterFactory({
 });
 
 describe("Druid requester live data source", function() {
-  this.timeout(5000);
+  this.timeout(10000);
 
   it("introspects single dataSource", () => {
     return toArray(druidRequester({
